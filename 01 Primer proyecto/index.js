ea8c75218ee21fcaaaea5833cc103891da677511
jdfioretti <jdfioretti@gmail.com>
2022-12-19 19:24:27 -0300

    unidad1-tarea1

---
+++ 01 Primer proyecto/index.js
@@ -0,0 +1,11 @@
+//Comentarios en JS --> es buena practica
+
+/* Este es el mensaje de bienvenida */
+console.log("Esta es la puerta de entrada al proyecto");
+
+/* Este es un comentario intermedio
+En esta parte realizamos los chequeos necesarios
+Por ultimo vamos a la linea de saludo */
+
+//Este es el mensaje de despedida
+console.log("Adios")
