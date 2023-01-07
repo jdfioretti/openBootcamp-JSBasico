aff9e42c361cd82d2352de6078a7f6b3fa33161d
jdfioretti <jdfioretti@gmail.com>
2023-01-07 09:34:58 -0300

    tarea 13

---
+++ 13 eslint/eslint-npm/index.js
@@ -0,0 +1,31 @@
+//Este es el archivo que vamos a utilizar
+
+
+//Queremos cambiar alguna regla:
+// agregamos esta linea al json --> "no-unused-vars": "off"
+
+const nombre = "Javier"
+
+//Podemos hacerlo solo en una linea
+
+/* eslint-disable */ //desactiva todo
+
+const nombre2 = 'Maria'
+
+/* eslint-enable */
+
+//En esta linea quiero tener comillas simples
+const nombre3 = 'este es un nuevo string'; /* eslint-disable-line */ 
+
+/* eslint-disable-next-line */ 
+const string2 = 'otro string';
+
+//////////////////
+//Podemos buscar configuraciones de eslint en npm. Hay cientos
+
+//console.log("hola")
+const name = "juan"
+//console.log("chau")
+
+//agregamos script al json para que repare automaticamente los errores
+
---
+++ 13 eslint/index.js
@@ -0,0 +1,10 @@
+const nombre = "Javier"
+
+    const nombre2 = "Maria";
+
+var objeto = {
+    nombre: "circulo",
+    radio: 2,
+    nombre: "cuadrado"
+}
+
